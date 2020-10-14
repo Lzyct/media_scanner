3065882c3e4183caca2b71bfc079d22dbd7b613b
ukieTux <ukie.tux@gmail.com>
2020-10-14 23:02:35 +0800

    Clean up code, add Comment

--- lib/media_scanner.dart
+++ lib/media_scanner.dart
@@ -3,11 +3,10 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class MediaScanner {
+  /// Define Method Channel
   static const MethodChannel _channel = const MethodChannel('media_scanner');
 
-  static Future<String> loadMedia({String path}) async {
-    var data = {"path": path};
-
-    return await _channel.invokeMethod('refreshGallery', data);
-  }
+  /// Path : Path of Image/Video
+  static Future<String> loadMedia({String path}) async =>
+      await _channel.invokeMethod('refreshGallery', {"path": path});
 }
